d4dd2b40b6edc5b23add6c9eda170b9a7ca422f0
geneyee <geneyeey@gmail.com>
2023-05-15 19:06:37 +0900

    feat : user 존재하지 않을 경우 예외처리

--- src/main/java/com/group/libraryapp/controller/user/UserController.java
+++ src/main/java/com/group/libraryapp/controller/user/UserController.java
@@ -2,13 +2,11 @@ package com.group.libraryapp.controller.user;
 
 import com.group.libraryapp.domain.user.User;
 import com.group.libraryapp.dto.user.request.UserCreateRequest;
+import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,10 +16,10 @@ import java.util.List;
 @RestController
 public class UserController {
 
-//    private final List<User> users = new ArrayList<>();
+    //    private final List<User> users = new ArrayList<>();
     private final JdbcTemplate jdbcTemplate;
 
-    public UserController(JdbcTemplate jdbcTemplate){
+    public UserController(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
 
@@ -48,5 +46,33 @@ public class UserController {
 
             return new UserResponse(id, name, age);
         });
+    }
+
+    @PutMapping("/user")
+    public void updateUser(@RequestBody UserUpdateRequest request) {
+        String readSql = "SELECT * FROM user WHERE id = ?";
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, request.getId()).isEmpty();
+        //결과가 있다면 0 -> 0있는 list 아니면 비어있는 list
+        //유저가 존재하지 않는 경우는 isEmpty()
+        //즉, 유저 있으면 0있으니까 false
+        //유저 없으면 비어있으니까 true
+        if (isUserNotExist) {//true면
+            throw new IllegalStateException();//실행-오류 뜨는 것
         }
+
+        String sql = "UPDATE user SET name = ? WHERE id = ?";
+        jdbcTemplate.update(sql, request.getName(), request.getId());
+    }
+
+    @DeleteMapping("/user")
+    public void deleteUser(@RequestParam String name) {
+        String readSql = "SELECT * FROM user WHERE name = ?";
+        boolean isUserNotExist = jdbcTemplate.query(readSql, (rs, rowNum) -> 0, name).isEmpty();
+
+        if (isUserNotExist) {
+            throw new IllegalStateException();
+        }
+        String sql = "DELETE FROM user WHERE name = ?";
+        jdbcTemplate.update(sql, name);
+    }
 }
