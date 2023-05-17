afa8581c222b1b311207e5b2d217ed3181790d4d
geneyee <geneyeey@gmail.com>
2023-05-17 15:02:16 +0900

    Entity 설정

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,9 +1,22 @@
 package com.group.libraryapp.domain.user;
 
+import javax.persistence.*;
+
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id = null;
+
+    @Column(nullable = false, length = 20) //name varchar(20)
     private String name;
+
     private Integer age;
+
+    //Entity 객체에는 매개변수가 하나도 없는 기본 생성자가 필요하다
+    //기본 생성자는 protected 해도 됨
+    protected User(){}
 
     public User(String name, Integer age) {
         if (name == null || name.isBlank()) {
