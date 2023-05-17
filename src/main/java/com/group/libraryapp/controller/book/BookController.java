4c706a5c62ef9936c9bc188ff037a5e6c4156417
geneyee <geneyeey@gmail.com>
2023-05-17 14:05:28 +0900

    Ioc/DI @Primay

--- src/main/java/com/group/libraryapp/controller/book/BookController.java
+++ src/main/java/com/group/libraryapp/controller/book/BookController.java
@@ -8,7 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     //스프링 빈이 아닌 경우 인스턴스화
-    private final BookService bookService = new BookService();
+    private final BookService bookService;
+
+    //Service 스프링빈 등록 시킨후(@Service) 생성자로 주입
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
 
     @PostMapping("/book")
     public void saveBook() {
--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -3,14 +3,22 @@ package com.group.libraryapp.service.book;
 import com.group.libraryapp.repository.book.BookMemoryRepository;
 import com.group.libraryapp.repository.book.BookMysqlRepository;
 import com.group.libraryapp.repository.book.BookRepository;
+import org.springframework.stereotype.Service;
 
 import java.awt.print.Book;
 
+@Service
 public class BookService {
 
     //private final BookMemoryRepository bookMemoryRepository = new BookMemoryRepository();
 
-    private final BookRepository bookRepository = new BookMysqlRepository();
+    private final BookRepository bookRepository;
+
+    //new를 통해 직접 어떤 repository를 쓸지 코드를 적는게 아니고 스프링컨테이너가 알아서 선택해줌 = IoC(제어의 역전)
+  //컨테이너가 특정 구현체를 선택해서 넣어주는 과정을 DI(의존성 주입)
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public void saveBook() {
        bookRepository.saveBook();
