5783ccc47c4f816011dfe73b196f1eea134c6b4f
geneyee <geneyeey@gmail.com>
2023-05-19 12:24:59 +0900

    @Transactional 추가

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,10 +20,12 @@ public class UserServiceV2 {
         this.userRepository = userRepository;
     }
 
+    @Transactional
     public void saveUser(UserCreateRequest request){
         userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers(){
         //List<User> users = userRepository.findAll();
 /*        return userRepository.findAll().stream()
@@ -34,6 +37,7 @@ public class UserServiceV2 {
                 .collect(Collectors.toList());
     }//UserResponse::new UserResponse에서 User user매개변수로 생성자 만든거
 
+    @Transactional
     public void updateUser(UserUpdateRequest request) {
         //select * from user where id = ?
         //Optional<User>
@@ -44,12 +48,24 @@ public class UserServiceV2 {
         userRepository.save(user);
     }
 
+    @Transactional
     public void deleteUser(String name){
+
         //select * from user where name = ?
        User user = userRepository.findByName(name)
                .orElseThrow(IllegalArgumentException::new);
 
        userRepository.delete(user);
+
+/*
+        boolean existsByName(String name);사용할 경우
+
+        if(!userRepository.existsByName(name)){
+           throw new IllegalArgumentException();
+       }
+*/
+
+
        }
     }
 
